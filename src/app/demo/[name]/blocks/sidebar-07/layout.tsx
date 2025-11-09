bfc8b46517ed51dff9078be0d85fa5b2d4da6bec
politokk <133917774+politokk@users.noreply.github.com>
2025-11-09 17:36:56 -0500

    rreeg

--- src/app/demo/[name]/blocks/sidebar-07/layout.tsx
+++ src/app/demo/[name]/blocks/sidebar-07/layout.tsx
@@ -1,18 +1,14 @@
 import type { Metadata } from "next"
 
-import { META_THEME_COLORS, siteConfig } from "@/lib/config"
-import { fontVariables } from "@/lib/fonts"
 import { cn } from "@/lib/utils"
-import { LayoutProvider } from "@/hooks/use-layout"
-import { Toaster } from "@/components/ui/sonner"
 import "@/app/globals.css"
 
 export const metadata: Metadata = {
   title: {
-    default: siteConfig.name,
-    template: `%s - ${siteConfig.name}`,
+    default: "Linderlabs",
+    template: `%s - Linderlabs`,
   },
-  description: siteConfig.description,
+  description: "A example registry for distributing code using shadcn.",
   keywords: ["Next.js", "React", "Tailwind CSS", "Components", "shadcn"],
   authors: [
     {
@@ -24,22 +20,22 @@ export const metadata: Metadata = {
   openGraph: {
     type: "website",
     locale: "en_US",
-    title: siteConfig.name,
-    description: siteConfig.description,
-    siteName: siteConfig.name,
+    title: "Linderlabs",
+    description: "A example registry for distributing code using shadcn.",
+    siteName: "Linderlabs",
     images: [
       {
         url: `${process.env.NEXT_PUBLIC_APP_URL}/opengraph-image.png`,
         width: 1200,
         height: 630,
-        alt: siteConfig.name,
+        alt: "Linderlabs",
       },
     ],
   },
   twitter: {
     card: "summary_large_image",
-    title: siteConfig.name,
-    description: siteConfig.description,
+    title: "Linderlabs",
+    description: "A example registry for distributing code using shadcn.",
     images: [`${process.env.NEXT_PUBLIC_APP_URL}/opengraph-image.png`],
     creator: "@linderlabs",
   },
@@ -48,7 +44,7 @@ export const metadata: Metadata = {
     shortcut: "/favicon-16x16.png",
     apple: "/apple-touch-icon.png",
   },
-  manifest: `${siteConfig.url}/site.webmanifest`,
+  manifest: `${process.env.NEXT_PUBLIC_APP_URL}/site.webmanifest`,
 }
 
 export default function RootLayout({
@@ -67,7 +63,7 @@ export default function RootLayout({
                 document.body.classList.add('theme-loading')
 
                 if (localStorage.theme === 'dark' || ((!('theme' in localStorage) || localStorage.theme === 'system') && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-                  document.querySelector('meta[name="theme-color"]').setAttribute('content', '${META_THEME_COLORS.dark}')
+                  document.querySelector('meta[name="theme-color"]').setAttribute('content', '#000000')
                 }
                 if (localStorage.layout) {
                   document.documentElement.classList.add('layout-' + localStorage.layout)
@@ -85,19 +81,15 @@ export default function RootLayout({
             `,
           }}
         />
-        <meta name="theme-color" content={META_THEME_COLORS.light} />
+        <meta name="theme-color" content="#ffffff" />
       </head>
       <body
-      suppressHydrationWarning
+        suppressHydrationWarning
         className={cn(
-          "text-foreground group/body theme-default overscroll-none font-sans antialiased [--footer-height:calc(var(--spacing)*14)] [--header-height:calc(var(--spacing)*14)] xl:[--footer-height:calc(var(--spacing)*24)]",
-          fontVariables
+          "text-foreground group/body theme-default overscroll-none font-sans antialiased [--footer-height:calc(var(--spacing)*14)] [--header-height:calc(var(--spacing)*14)] xl:[--footer-height:calc(var(--spacing)*24)]"
         )}
       >
-          <LayoutProvider>
-              {children}
-              <Toaster position="top-center" />
-          </LayoutProvider>
+        {children}
       </body>
     </html>
   )
